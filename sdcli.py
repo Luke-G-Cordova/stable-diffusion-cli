066719b1afd46b8bdba0f2114d9ce35f43df693a
Luke Cordova <lukethegreat000@gmail.com>
2023-12-15 18:27:52 -0800

    most cli built out

---
+++ sdcli.py
@@ -0,0 +1,194 @@
+import argparse
+from diffusers.pipelines.stable_diffusion.convert_from_ckpt import download_from_original_stable_diffusion_ckpt
+from os import path
+import os
+import txt2img
+import colors as co
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--model_path",
+    default="./models/stable-diffusion-v2",
+    type=str,
+    help="Path to the base model to run stable diffusion on. If evaluates to .ckpt or .safetensor file will convert to and use a huggingface diffusers model stored in ./models"
+)
+parser.add_argument(
+    "--scheduler_type",
+    default="pndm",
+    type=str,
+    help="scheduler type. One of ['pndm', 'lms', 'ddim', 'euler', 'euler-ancestral', 'dpm']"
+)
+parser.add_argument(
+    "--embeddings_path",
+    default="models/embeddings",
+    type=str,
+    help="Path to directory containing embedding files."
+)
+parser.add_argument(
+    "--prompt",
+    default=None,
+    type=str,
+    help="text prompt"
+)
+parser.add_argument(
+    "--negative_prompt",
+    default=None,
+    type=str,
+    help="negative text prompt"
+)
+parser.add_argument(
+    "--prompt_file",
+    default="prompts/p.txt",
+    type=str,
+    help="A file storing the prompt. --prompt flag must not be set to use this"
+)
+parser.add_argument(
+    "--negative_prompt_file",
+    default="prompts/n.txt",
+    type=str,
+    help="A file storing the negative prompt. --negative_prompt flag must not be set to use this"
+)
+parser.add_argument(
+    "--seed",
+    default=-1,
+    type=int,
+    help="Seed. If seed is -1 a random seed will be generated and used"
+)
+parser.add_argument(
+    "--clip_skip",
+    default=1,
+    type=int,
+    help="clip skip"
+)
+parser.add_argument(
+    "--inference_steps",
+    default=30,
+    type=int,
+    help="amount of inference steps"
+)
+parser.add_argument(
+    "--guidance_scale",
+    default=7,
+    type=float,
+    help="guidance scale"
+)
+parser.add_argument(
+    "--width",
+    default=512,
+    type=int,
+    help="width of output image"
+)
+parser.add_argument(
+    "--height",
+    default=512,
+    type=int,
+    help="height of output image"
+)
+parser.add_argument(
+    "--task",
+    default="txt2img",
+    type=str,
+    help="Task to preform, txt2img, img2img"
+)
+parser.add_argument(
+    "--lora_path",
+    default="models/lora",
+    type=str,
+    help="path to directory storing lora"
+)
+parser.add_argument(
+    "--batch_size",
+    default=1,
+    type=int,
+    help="batch size"
+)
+
+args = parser.parse_args()
+
+model_path = args.model_path
+if path.isdir(model_path):
+    print(f"{co.neutral}Model path: {co.blue}{model_path} {co.green}: DETECTED{co.reset}")
+elif path.isfile(model_path):
+    fname, ext = path.splitext(model_path)
+    new_path = path.join(path.abspath("./models/"), fname)
+    if path.isdir(new_path):
+        print(f"{co.blue}{new_path} {co.neutral}has already been created.\nUsing {co.blue}{new_path}{co.neutral} as model.{co.reset}")
+        model_path = new_path
+    else:
+        print(f"{co.neutral}Attempting to convert {co.blue}{model_path}{co.neutral} to huggingface model{co.reset}")
+        from_safetensors = True if ext == ".safetensors" else False
+
+        pipe = download_from_original_stable_diffusion_ckpt(
+            checkpoint_path_or_dict=model_path,
+            original_config_file=None,
+            image_size=None,
+            prediction_type=None,
+            model_type=None,
+            extract_ema=False,
+            scheduler_type=args.scheduler_type,
+            num_in_channels=None,
+            upcast_attention=False,
+            from_safetensors=from_safetensors,
+            device="cuda",
+            stable_unclip=None,
+            stable_unclip_prior=None,
+            clip_stats_path="",
+            controlnet=False,
+            vae_path=None,
+            pipeline_class=None,
+        )
+        # if args.half:
+        #     pipe.to(torch_dtype=torch.float16)
+
+        # if args.controlnet:
+        #     # only save the controlnet model
+        #     pipe.controlnet.save_pretrained(args.dump_path, safe_serialization=args.to_safetensors)
+        # else:
+        model_path = new_path
+        pipe.save_pretrained(model_path, safe_serialization=True)
+else:
+    print(f"{co.neutral}Model path: {co.blue}{model_path}{co.neutral} :does not exist{co.reset}")
+    exit()
+
+embeddings_path = args.embeddings_path
+if path.isdir(embeddings_path):
+    print(f"{co.neutral}Embedding path: {co.blue}{embeddings_path}{co.green} : DETECTED{co.reset}")
+else:
+    print(f"{co.yellow}Embeddings path not found{co.reset}")
+
+if args.prompt is None: 
+    if path.isfile(args.prompt_file):
+        print(f"{co.neutral}Prompt path: {co.blue}{args.prompt_file}{co.green} : DETECTED{co.reset}")
+        with open(args.prompt_file, "r") as f:
+            args.prompt = f.read().strip()
+    else:
+        raise ValueError(f"{co.red}--prompt was left unset and can not find {args.prompt_file}{co.reset}")
+if args.negative_prompt is None: 
+    if path.isfile(args.negative_prompt_file):
+        print(f"{co.neutral}Prompt path: {co.blue}{args.negative_prompt_file}{co.green} : DETECTED{co.reset}")
+        with open(args.negative_prompt_file, "r") as f:
+            args.negative_prompt = f.read().strip()
+    else:
+        raise ValueError(f"{co.red}--negative_prompt was left unset and can not find {args.negative_prompt_file}{co.reset}")
+
+
+if args.task == "txt2img":
+    txt2img.start(
+        model_path=model_path,
+        scheduler_type=args.scheduler_type,
+        prompt=args.prompt,
+        negative_prompt=args.negative_prompt,
+        embeddings_path=embeddings_path,
+        width=args.width,
+        height=args.height,
+        seed=args.seed,
+        clip_skip=args.clip_skip,
+        lora_path=args.lora_path,
+        num_inference_steps=args.inference_steps,
+        guidance_scale=args.guidance_scale,
+        batch_size=args.batch_size
+    )
+else:
+    print(f"{co.red}No current support for {args.task}{co.reset}")
+
---
+++ txt2img.py
@@ -0,0 +1,173 @@
+from diffusers import(
+  DiffusionPipeline
+)
+import os
+from os import path
+from safetensors.torch import load_file
+import torch
+import random
+import re
+import tqdm
+import colors as co
+
+torch.backends.cuda.matmul.allow_tf32 = True
+
+
+# ['pndm', 'lms', 'ddim', 'euler', 'euler-ancestral', 'dpm']
+def get_scheduler_import(scheduler_type):
+    if scheduler_type == "pndm":
+        from diffusers import PNDMScheduler as sc
+    elif scheduler_type == "lms":
+        from diffusers import LMSDiscreteScheduler as sc
+    elif scheduler_type == "heun":
+        from diffusers import HeunDiscreteScheduler as sc
+    elif scheduler_type == "euler":
+        from diffusers import EulerDiscreteScheduler as sc
+    elif scheduler_type == "euler-ancestral":
+        from diffusers import EulerAncestralDiscreteScheduler as sc
+    elif scheduler_type == "dpm":
+        from diffusers import DPMSolverMultistepScheduler as sc
+    elif scheduler_type == "ddim":
+        from diffusers import DDIMScheduler as sc
+    else:
+        raise ValueError(f"Scheduler of type {scheduler_type} doesn't exist!")
+    return sc
+
+def parse_prompt(prompt):
+    pieces = prompt.split(",")
+
+    # detect lora
+    name = []
+    weight = []
+    i = 0
+    while i < len(pieces):
+        s = re.findall("\<lora:.+:[0-9.]+\>", pieces[i])
+        if len(s) > 0:
+            for mat in s:
+                # get lora name
+                mat = mat[1:]
+                mat = mat[:-1]
+                lora_info = mat.split(":")
+                name.append(lora_info[1])
+                weight.append(float(lora_info[2]))
+            pieces.remove(pieces[i])
+        else:
+            i+=1
+    return ",".join(pieces), name, weight
+
+def add_lora(
+    pipe,
+    lora_names,
+    lora_weights,
+    lora_path
+):
+    if path.isdir(lora_path):
+        for i, name in enumerate(lora_names):
+            if path.isfile(path.join(lora_path, name + ".safetensors")):
+                pipe.load_lora_weights(
+                    path.join(lora_path, name + ".safetensors"),
+                    weight_name=name+".safetensors",
+                    adapter_name=name,
+                )
+                print(f"{co.neutral}LoRA file: {co.blue}{path.join(lora_path, name + '.safetensors')}{co.green} : DETECTED{co.reset}")
+            elif path.isfile(path.join(lora_path, name + ".ckpt")):
+                pipe.load_lora_weights(
+                    path.join(lora_path, name + ".ckpt"),
+                    weight_name=name+".ckpt",
+                    adapter_name=name,
+                )
+                print(f"{co.neutral}LoRA file: {co.blue}{path.join(lora_path, name + '.ckpt')}{co.green} : DETECTED{co.reset}")
+            else:
+                print(f"{co.neutral}LoRA file: {co.blue}{path.join(lora_path, name +'.safetensors')}{co.yellow} : UNDETECTED {co.neutral}Will not use this lora{co.reset}")
+                del lora_names[i]
+                del lora_weights[i]
+    else:
+        raise ValueError(f"{co.red}{lora_path} is not a path to a lora directory{co.reset}")
+
+    pipe.set_adapters(lora_names, lora_weights)
+
+
+def start(
+    model_path, 
+    scheduler_type="pndm",
+    embeddings_path="",
+    prompt="",
+    negative_prompt="",
+    seed=-1,
+    clip_skip=1,
+    lora_path="",
+    num_inference_steps=20,
+    guidance_scale=7,
+    width=512,
+    height=512,
+    batch_size=1,
+):
+    if torch.cuda.is_available():
+        device_name = torch.device("cuda")
+    else:
+        device_name = torch.device("cpu")
+    print(f"{co.neutral}Using device: {co.blue}{device_name}{co.neutral} : for inference{co.reset}")
+
+    scheduler = get_scheduler_import(scheduler_type).from_pretrained(
+        model_path,
+        subfolder="scheduler"
+    )
+    pipe = DiffusionPipeline.from_pretrained(
+        model_path,
+        scheduler=scheduler,
+        safety_checker=None,
+        use_safetensors=True
+    )
+    if path.isdir(embeddings_path):
+        for filename in os.listdir(embeddings_path):
+            fn, ext = path.splitext(filename)
+            if ext == ".safetensors":
+                embed_file = load_file(path.join(embeddings_path, filename), device="cpu")
+                pipe.load_textual_inversion(embed_file["emb_params"], token=fn, text_encoder=pipe.text_encoder, tokenizer=pipe.tokenizer)
+            else:
+                embed_file = torch.load(path.join(embeddings_path, filename), map_location="cpu")
+                pipe.load_textual_inversion(embed_file["string_to_param"]["*"], token=fn, text_encoder=pipe.text_encoder, tokenizer=pipe.tokenizer)
+            print(f"{co.neutral}Embedding: {co.blue}{path.join(embeddings_path, filename)}{co.green} : DETECTED{co.reset}")
+    pipe.to(device_name)
+
+    if seed == -1:
+        seed = random.randrange(0, 1000000)
+    generator = torch.Generator(device="cuda").manual_seed(seed)
+    
+    prompt, pLora, pWeight = parse_prompt(prompt)
+    negative_prompt, nLora, nWeight = parse_prompt(negative_prompt)
+
+    print(f"{co.neutral}PROMPT: {co.reset}{prompt}")
+    print(f"{co.neutral}NEGATIVE_PROMPT: {co.reset}{negative_prompt}")
+
+    add_lora(pipe=pipe, lora_names=pLora+nLora, lora_weights=pWeight+nWeight, lora_path=lora_path)
+
+    clip_layers = pipe.text_encoder.text_model.encoder.layers
+
+    for _ in range(batch_size):
+        if clip_skip > 1:
+            pipe.text_encoder.text_model.encoder.layers = clip_layers[:-(clip_skip)]
+        image = pipe(
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=guidance_scale,
+            generator=generator,
+            num_images_per_prompt=1,
+            width=width,
+            height=height
+        ).images[0]
+
+        if clip_skip > 1:
+            pipe.text_encoder.text_model.encoder.layers = clip_layers
+
+        print(f"{co.green}saving to output/{seed}.png{co.reset}")
+        image.save(f"output/{seed}.png")
+
+        
+
+
+
+
+    
+
